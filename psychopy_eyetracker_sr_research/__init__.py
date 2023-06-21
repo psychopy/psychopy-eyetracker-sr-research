d7cbb8f7c9dabd46632d0dccde3fdea61fde6891
Matthew D. Cutone <mcutone@opensciencetools.org>
2023-06-21 13:48:32 -0400

    MISC: Initial commit

---
+++ psychopy_eyetracker_sr_research/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of the PsychoPy library
+# Copyright (C) 2012-2020 iSolver Software Solutions (C) 2021 Open Science Tools Ltd.
+# Distributed under the terms of the GNU General Public License (GPL).
+
+"""Extension package for PsychoPy which adds support for various hardware
+devices by SR Research.
+"""
+
+__version__ = "0.0.1"
