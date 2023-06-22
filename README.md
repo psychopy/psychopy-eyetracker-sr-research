1bdac42380d6010b5e1624b34069d27c10b967c6
Matthew D. Cutone <mcutone@opensciencetools.org>
2023-06-22 15:57:24 -0400

    FF: misc changes

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # psychopy-eyetracker-sr-research
-Add support to PsychoPy for SR Reseearch eyetrackers
+
+Extension for PsychoPy which adds support for [SR Research](https://www.sr-research.com/) 
+eyetrackers (via ioHub)
+
+## Supported Devices
+
+Installing this package alongside PsychoPy will enable support for the following 
+devices:
+
+* Supported SR Research EyeLink devices
+    
+## Installing
+
+Install this package with the following shell command:: 
+
+    pip install psychopy-eyetracker-sr-research
+
+You may also use PsychoPy's builtin plugin/package manager to install this 
+package.
+
+## Usage
+
+Once the package is installed, PsychoPy will automatically load it when started 
+and the `psychopy.iohub.devices.eyetracker.hw.sr_research` namespace will 
+contain the loaded objects.
--- psychopy_eyetracker_sr_research/sr_research/eyelink/__init__.py
+++ psychopy_eyetracker_sr_research/sr_research/eyelink/__init__.py
@@ -2,7 +2,26 @@
 # Part of the PsychoPy library
 # Copyright (C) 2012-2020 iSolver Software Solutions (C) 2021 Open Science Tools Ltd.
 # Distributed under the terms of the GNU General Public License (GPL).
-from .eyetracker import (EyeTracker, MonocularEyeSampleEvent,
-                        BinocularEyeSampleEvent, FixationStartEvent,
-                        FixationEndEvent, SaccadeStartEvent,
-                        SaccadeEndEvent, BlinkStartEvent,BlinkEndEvent)
+
+__all__ = [
+    'EyeTracker',
+    'MonocularEyeSampleEvent',
+    'BinocularEyeSampleEvent',
+    'FixationStartEvent',
+    'FixationEndEvent',
+    'SaccadeStartEvent',
+    'SaccadeEndEvent',
+    'BlinkStartEvent',
+    'BlinkEndEvent'
+]
+
+from .eyetracker import (
+    EyeTracker, 
+    MonocularEyeSampleEvent,
+    BinocularEyeSampleEvent, 
+    FixationStartEvent,
+    FixationEndEvent, 
+    SaccadeStartEvent,
+    SaccadeEndEvent, 
+    BlinkStartEvent,
+    BlinkEndEvent)
