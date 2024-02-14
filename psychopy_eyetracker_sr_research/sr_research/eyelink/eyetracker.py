d2e7eedf51dcffd75760aaadbafc1767e61c5791
Matthew D. Cutone <mcutone@opensciencetools.org>
2024-02-14 09:35:47 -0500

    BF: Removed `_unregisterEventMonitors` call

--- psychopy_eyetracker_sr_research/sr_research/eyelink/eyetracker.py
+++ psychopy_eyetracker_sr_research/sr_research/eyelink/eyetracker.py
@@ -427,7 +427,7 @@ class EyeTracker(EyeTrackerDevice):
             # {'message': 'calibration_result: 0', 'result': 1000}
             # on a successful calibration.
 
-            genv._unregisterEventMonitors()
+            # genv._unregisterEventMonitors()
             genv.clearAllEventBuffers()
             genv.window.close()
             del genv.window
